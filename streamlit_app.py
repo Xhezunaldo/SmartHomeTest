66153b445495331d1408530640f1e1fa2d3eb734
Xhezunaldo <141322274+Xhezunaldo@users.noreply.github.com>
2025-01-13 20:23:32 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,7 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
-import requests 
 from streamlit_autorefresh import st_autorefresh
-
-from function.anedya import anedya_config
-from function.anedya import anedya_sendCommand
-from function.anedya import anedya_getValue
-from function.anedya import anedya_setValue
-from function.anedya import fetchHumidityData
-from function.anedya import fetchTemperatureData
-
-nodeId = "4229885e-3456-11ef-9ecc-a1461caa74a3"  # get it from anedya dashboard -> project -> node 
-apiKey = "829d921000ee5204aa1cdb4fe4d2002fe7bbbe2c157983dad9bd7658f40d7229"  # anedya project apikey
 
 st.set_page_config(page_title="SMART HOME", layout="wide")
 
@@ -29,7 +18,6 @@ humidityData = pd.DataFrame()
 temperatureData = pd.DataFrame()
 
 def main():
-    anedya_config(nodeId, apiKey)
     global humidityData, temperatureData
 
     # Initialize the log in state if does not exist
@@ -57,12 +45,6 @@ def main():
     if st.session_state.LoggedIn is False:
         drawLogin()
     else:
-        humidityData = fetchHumidityData()
-        temperatureData = fetchTemperatureData()
-
-        GetLED1Status()
-        GetLED2Status()
-
         drawDashboard()
 
 def drawLogin():
@@ -82,7 +64,7 @@ def drawLogin():
             else:
                 st.error("Invalid Credential!")
     with cols[2]:
-        print()
+        pass
 
 def drawDashboard():
     headercols = st.columns([1, 0.1, 0.1], gap="small")
@@ -97,14 +79,14 @@ def drawDashboard():
         st.session_state.LoggedIn = False
         st.rerun()
 
-    st.markdown("<p style='color: navy;'>This dashboard provides a Smart homme Data information , also allowing you to control the LEDs !</p>", unsafe_allow_html=True)
+    st.markdown("<p style='color: navy;'>This dashboard provides a Smart Home data overview, also allowing you to control the LEDs!</p>", unsafe_allow_html=True)
 
     st.subheader(body="Current Status", anchor=False)
     cols = st.columns(2, gap="medium")
     with cols[0]:
         st.metric(label="Humidity", value=str(st.session_state.CurrentHumidity) + " %")
     with cols[1]:
-        st.metric(label="Temperature", value=str(st.session_state.CurrentTemperature) + "  °C")
+        st.metric(label="Temperature", value=str(st.session_state.CurrentTemperature) + " °C")
 
     buttons = st.columns(2, gap="small")
     with buttons[0]:
@@ -114,130 +96,30 @@ def drawDashboard():
         st.text("Control LED 2:")
         st.button(label=st.session_state.LED2ButtonText, on_click=operateLED2)
 
-    charts = st.columns(2, gap="small")
-    with charts[0]:
-        st.subheader(body="Humidity", anchor=False)
-        if humidityData.empty:
-            st.write("No Data Available!")
-        else:
-            humidity_chart_an = alt.Chart(data=humidityData).mark_area(
-                line={'color': '#1fa2ff'},
-                color=alt.Gradient(
-                    gradient='linear',
-                    stops=[alt.GradientStop(color='#1fafff', offset=1),
-                        alt.GradientStop(color='rgba(255,255,255,0)', offset=0)],
-                    x1=1,
-                    x2=1,
-                    y1=1,
-                    y2=0,
-                ),
-                interpolate='monotone',
-                cursor='crosshair'
-            ).encode(
-                x=alt.X(
-                    shorthand="Datetime:T",
-                    axis=alt.Axis(format="%Y-%m-%d %H:%M:%S", title="Datetime", tickCount=10, grid=True, tickMinStep=5),
-                ),  # T indicates temporal (time-based) data
-                y=alt.Y(
-                    "aggregate:Q",
-                    scale=alt.Scale(domain=[20, 60]),
-                    axis=alt.Axis(title="Humidity (%)", grid=True, tickCount=10),
-                ),  # Q indicates quantitative data
-                tooltip=[alt.Tooltip('Datetime:T', format="%Y-%m-%d %H:%M:%S", title="Time",),
-                        alt.Tooltip('aggregate:Q', format="0.2f", title="Value")],
-            ).properties(height=400).interactive()
-
-            # Display the Altair chart using Streamlit
-            st.altair_chart(humidity_chart_an, use_container_width=True)
-
-    with charts[1]:
-        st.subheader(body="Temperature", anchor=False)
-        if temperatureData.empty:
-            st.write("No Data Available!")
-        else:
-            temperature_chart_an = alt.Chart(data=temperatureData).mark_area(
-                line={'color': '#1fa2ff'},
-                color=alt.Gradient(
-                    gradient='linear',
-                    stops=[alt.GradientStop(color='#1fafff', offset=1),
-                        alt.GradientStop(color='rgba(255,255,255,0)', offset=0)],
-                    x1=1,
-                    x2=1,
-                    y1=1,
-                    y2=0,
-                ),
-                interpolate='monotone',
-                cursor='crosshair'
-            ).encode(
-                x=alt.X(
-                    shorthand="Datetime:T",
-                    axis=alt.Axis(format="%Y-%m-%d %H:%M:%S", title="Datetime", tickCount=10, grid=True, tickMinStep=5),
-                ),  # T indicates temporal (time-based) data
-                y=alt.Y(
-                    "aggregate:Q",
-                    # scale=alt.Scale(domain=[0, 100]),
-                    scale=alt.Scale(zero=False, domain=[10, 50]),
-                    axis=alt.Axis(title="Temperature (°C)", grid=True, tickCount=10),
-                ),  # Q indicates quantitative data
-                tooltip=[alt.Tooltip('Datetime:T', format="%Y-%m-%d %H:%M:%S", title="Time",),
-                        alt.Tooltip('aggregate:Q', format="0.2f", title="Value")],
-            ).properties(height=400).interactive()
-
-            st.altair_chart(temperature_chart_an, use_container_width=True)
+    # Data input fields for testing
+    st.subheader("Set Data Values for Testing")
+    st.session_state.CurrentHumidity = st.number_input("Set Humidity (%)", min_value=0, max_value=100, value=st.session_state.CurrentHumidity)
+    st.session_state.CurrentTemperature = st.number_input("Set Temperature (°C)", min_value=-50, max_value=50, value=st.session_state.CurrentTemperature)
 
 def operateLED1():
     if st.session_state.LED1State is False:
-        anedya_sendCommand("LED1", "ON")
-        anedya_setValue("LED1", True)
         st.session_state.LED1ButtonText = "LED 1 Off!"
         st.session_state.LED1State = True
         st.toast("LED 1 on!")
     else:
         st.session_state.LED1ButtonText = "LED 1 On!"
         st.session_state.LED1State = False
-        anedya_sendCommand("LED1", "OFF")
-        anedya_setValue("LED1", False)
         st.toast("LED 1 off!")
 
 def operateLED2():
     if st.session_state.LED2State is False:
-        anedya_sendCommand("LED2", "ON")
-        anedya_setValue("LED2", True)
         st.session_state.LED2ButtonText = "LED 2 Off!"
         st.session_state.LED2State = True
         st.toast("LED 2 on!")
     else:
         st.session_state.LED2ButtonText = "LED 2 On!"
         st.session_state.LED2State = False
-        anedya_sendCommand("LED2", "OFF")
-        anedya_setValue("LED2", False)
         st.toast("LED 2 off!")
-
-@st.cache_data(ttl=4, show_spinner=False)
-def GetLED1Status() -> list:
-    value = anedya_getValue("LED1")
-    if value[1] == 1:
-        on = value[0]
-        if on:
-            st.session_state.LED1State = True
-            st.session_state.LED1ButtonText = "LED 1 Off!"
-        else:
-            st.session_state.LED1State = False
-            st.session_state.LED1ButtonText = "LED 1 On!"
-    return value
-
-@st.cache_data(ttl=4, show_spinner=False)
-def GetLED2Status() -> list:
-    value = anedya_getValue("LED2")
-    if value[1] == 1:
-        on = value[0]
-        if on:
-            st.session_state.LED2State = True
-            st.session_state.LED2ButtonText = "LED 2 Off!"
-        else:
-            st.session_state.LED2State = False
-            st.session_state.LED2ButtonText = "LED 2 On!"
-    return value
 
 if __name__ == "__main__":
     main()
